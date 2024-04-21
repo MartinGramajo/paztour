76931c414e2fde925123311084b45e9d2ad5bc65
¨mago¨ <martingramajo08@gmail.com>
2024-04-21 19:33:54 -0300

    probando

--- src/components/MetaverseCel.jsx
+++ src/components/MetaverseCel.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Image } from "react-bootstrap";
 import metaverse from "../assets/img/a1.jpg";
-import cieloLimite from "../assets/img/cielolimite.png";
+import agencia from "../assets/img/cielolimite.png";
 
 export default function MetaverseCel() {
   return (
@@ -9,21 +9,11 @@ export default function MetaverseCel() {
       <div className="container ">
         <div className="row">
           <div className="my-5 ">
-            <div className="col-12">
-              <Image
-                className="h-100"
-                src={cieloLimite}
-                alt="banner-about"
-                fluid
-              />
+            <div className="col-lg-6 mx-3">
+              <Image className="" src={agencia} alt="banner-about" fluid />
             </div>
-            <div className="col-12">
-              <Image
-                className="h-100"
-                src={metaverse}
-                alt="banner-about"
-                fluid
-              />
+            <div className="col-lg-6 mx-3">
+              <Image className="" src={metaverse} alt="banner-about" fluid />
             </div>
           </div>
         </div>
