869dde4ba180681320e5719562ad1364aac80d7c
¨mago¨ <martingramajo08@gmail.com>
2024-04-21 19:32:43 -0300

    haciendo pruebas

--- src/components/AskedCel.jsx
+++ src/components/AskedCel.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Image } from "react-bootstrap";
+// import cieloLimite from "../assets/img/movil.png";
 import cieloLimite from "../assets/img/horariosatencion.png";
 
 export default function AskedCel() {
@@ -8,15 +9,10 @@ export default function AskedCel() {
       <div className="container ">
         <div className="row">
           <div className="my-5 ">
-            <div className="mx-3">
-              <Image
-                className="h-100"
-                src={cieloLimite}
-                alt="banner-about"
-                fluid
-              />
+            <div className="col-lg-6 mx-3">
+              <Image className="" src={cieloLimite} alt="banner-about" fluid />
             </div>
-            <div className=" mx-3">
+            <div className=" mx-3 col-lg-6 ">
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3135.1385980453456!2d-1.0414942!3d38.2066788!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd639114d72c04cb%3A0xfe3710a5443e2ba!2sPaztour%20Agency%20turismo%20y%20formacion!5e0!3m2!1ses-419!2sar!4v1711841045143!5m2!1ses-419!2sar"
                 className="w-100"
